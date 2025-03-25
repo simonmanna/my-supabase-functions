aebe92d9dbf32b90407bd427e5f941dfa1ef0a3e
Simon Okbagergis Manna <32343117+saimenogm@users.noreply.github.com>
2025-03-25 15:47:33 +0300

    Add Supabase Edge Function4

--- supabase/functions/project1/index.ts
+++ supabase/functions/project1/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
 serve((req) => {
-  return new Response("Hello from project1!", { status: 200 });
+  return new Response("Updated project1 via GitHub Actions!", { status: 200 });
 });
